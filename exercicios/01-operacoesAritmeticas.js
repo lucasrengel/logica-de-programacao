028cec5b4781876bf796d007a6d4fade51196673
lucas rengel <lucas_rengel@estudante.sc.senai.br>
2023-05-08 17:59:32 -0300

    new file:   exercicios/01-operacoesAritmeticas.js

---
+++ exercicios/01-operacoesAritmeticas.js
@@ -0,0 +1,12 @@
+let num1 = 2;
+let num2 = 4;
+
+const soma = num1 + num2;
+const subtracao = num1 - num2;
+const divisao = num1 / num2;
+const multiplicacao = num1 * num2;
+
+console.log("Soma: " + num1 + " + " + num2 + " = " + soma);
+console.log("Subtracao: " + num1 + " - " + num2 + " = " + subtracao);
+console.log("Divisao: " + num1 + " / " + num2 + " = " + divisao);
+console.log("Multiplicacao: " + num1 + " * " + num2 + " = " + multiplicacao);
