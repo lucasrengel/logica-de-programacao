f706f359a195f34648329490db048ec24a162de8
lucas rengel <lucas_rengel@estudante.sc.senai.br>
2023-05-09 17:10:55 -0300

    modified:   exercicios/02-valorJantar.js

--- exercicios/02-valorJantar.js
+++ exercicios/02-valorJantar.js
@@ -4,4 +4,4 @@ let totalJantar = valorJantar + taxaGarcom;
 
 console.log("Valor total da jantar: R$" + valorJantar.toFixed(2));
 console.log("Valor total da taxa do Garçom: R$" + taxaGarcom.toFixed(2));
-console.log("Valor total a pagar incluso com a taxa do garçom: R$" + totalJantar.toFixed(2));
+console.log("Valor total a pagar incluso com a taxa do garçom: R$" + totalJantar.toFixed(2)); // .toFixed ou (totalJantar + ",00")
