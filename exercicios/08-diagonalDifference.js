7a1ced7ffde91637af5ff20bcf67a4243b4ecbc6
lucas rengel <lucas_rengel@estudante.sc.senai.br>
2023-05-21 23:31:12 -0300

    new file:   exercicios/07-aVeryBigSum.js new file:   exercicios/08-diagonalDifference.js

---
+++ exercicios/08-diagonalDifference.js
@@ -0,0 +1,15 @@
+function diagonalDifference(arr){
+  let lDig = 0;
+  let rDig = 0;
+
+  for(let index = 0; index < arr.length; index++){
+    lDig += arr[index][index];
+    rDig += arr[index][arr.length - 1 - index];
+  }
+  return Math.abs(lDig - rDig);
+}
+
+console.log(diagonalDifference(
+  [[1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9]]));
